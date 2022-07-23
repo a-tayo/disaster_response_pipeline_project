e1b3cafd27aa6bceb08b3f36eb8f7fb2561a4172
a-tayo <tayos133@gmail.com>
2022-07-23 22:10:45 +0100

    Add:  Add process_data .py core functionality

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,90 @@
+# import libraries
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Read in two csv files into dataframes, merges them 
+       and return a single dataframe
+    
+    Parameters
+    ----------- 
+    messages_filepath(str): 
+            filepath to the csv file containing messages
+           
+    categories_filepath(str):
+            filepath to the csv file containing the categories
+
+    Returns
+    --------
+    Merged Dataframe containing both the categories and the messages 
+    """
+    # read in the data to a pandas dataframe
+    messages =pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    # merge the cateogries and messages dataframe on common id
+    df = pd.merge(messages, categories, on='id')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Takes a dataframe and performs cleaning operation
+        on it
+
+    Parameters
+    -----------
+    df:
+        dataframe to clean
+    
+    Return
+    ------
+        cleaned dataframe
+    """
+
+    categories = df.categories.str.split(";", expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
+
+    # extract a list of new column names for categories
+    category_colnames = row.apply(lambda x: x[:-2])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    # convert categories 
+    categories = categories.applymap(lambda x:int(x[-1:]))
+
+    # drop the rows from which have a value of 2 for the related cateogry
+    categories.drop(categories[categories.iloc[:,0]==2].index, inplace=True)
+
+    # drop the original categories column from `df`
+    df.drop('categories', inplace=True, axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.merge(df, categories, right_index=True, left_index=True)
+
+    # drop duplicates
+    df.drop(df[df.duplicated()].index, inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Saves the into a sqlite database file
+
+    Parameters
+    ----------
+    df:
+        dataframe to save
+    database_filename:
+        filename to save the data in"
+    """
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
