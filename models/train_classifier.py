b558ee6628690143847847cb9a52f6d9a97bbb33
a-tayo <tayos133@gmail.com>
2022-07-24 14:04:27 +0100

    Add: add core functionality to train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,27 +1,146 @@
+# download necessary NLTK data
+import nltk
+nltk.download(['punkt', 'wordnet', 'omw-1.4'])
+
+# import libraries
 import sys
+import re
+import joblib
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import (CountVectorizer, 
+                                             TfidfTransformer)
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import GridSearchCV
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 
 def load_data(database_filepath):
-    pass
+    """
+    Loads the cleaned data from the database into memory in a pandas dataframe
 
+    Parameters
+    ----------
+    database_filepath(str): The path to the database file 
+    
+    Return
+    ------
+    X(numpy Array(str)): Array of strings of messages
+    Y(numpy Array(int)): Array of message categories 36 in total
+    category_names: Array of all categories labels 
+    """
+    # load data from database
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table('disaster_messages', engine)
+
+    # split into predictor and response variables
+    X = df.loc[:,'message'].values
+    Y = df.iloc[:,4:].values
+    category_names = df.iloc[:,4:].columns
+
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    """
+    Tokenizes a text into tokens
+
+    Parameters
+    ----------
+    text(str): The text to tokenize
+    
+    Returns
+    -------
+    tokens(list): List of cleaned message tokens  
+    """
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """
+    Builds the model and fine tune it with a grid search
+
+    Returns
+    -------
+    model(Model): The model object
+    """
+    # initialize a random forest classifier
+    rf = RandomForestClassifier(random_state=42, n_jobs=-1)
+    # make a data processing pipeline
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(rf, n_jobs=-1) )
+    ])
+
+    parameters = {
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [20, 50, 60]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1, cv=2)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Evaluates the model on the test set
+
+    Parameters
+    ----------
+    model(Model): The model object
+    X_test(numpy.ndarray): The training dataset
+    Y_test(numpy Array): The expected response
+    """
+    Y_pred = model.predict(X_test)
+    for i in range(len(category_names)):
+        print()
+        print(f'Feature {i+1}:',category_names[i],'\n',classification_report(Y_test[:,i], Y_pred[:,i]))
+    print('Accuracy', (Y_test == Y_pred).mean().mean())
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Saves the model to a file
+
+    Parameters
+    ----------
+    model(Model): The model object
+    model_filepath: str
+    """
+    joblib.dump(model, model_filepath)
 
 
 def main():
+    """
+    Main function
+    """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
