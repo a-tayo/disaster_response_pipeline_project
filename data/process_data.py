241601fd8a2733a0842bd8b0a070a63125487595
Aratunde Temitayo <35546618+a-tayo@users.noreply.github.com>
2022-07-25 22:20:36 +0100

    fix: fix error when database table already exist

--- data/process_data.py
+++ data/process_data.py
@@ -84,7 +84,7 @@ def save_data(df, database_filename):
         filename to save the data in"
     """
     engine = create_engine(f"sqlite:///{database_filename}")
-    df.to_sql('disaster_messages', engine, index=False)
+    df.to_sql('disaster_messages', engine, index=False, if_exists='replace')
 
 
 def main():
